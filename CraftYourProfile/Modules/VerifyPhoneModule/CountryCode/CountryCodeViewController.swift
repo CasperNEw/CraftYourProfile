2df34a1aadffff6880661f6b076280f261c786b6
CasperNEw <CasperNEW@inbox.ru>
2020-07-31 19:57:29 +0300

    update Issues, 12 points left out of 38. So cool =)

--- CraftYourProfile/Modules/VerifyPhoneModule/CountryCode/CountryCodeViewController.swift
+++ CraftYourProfile/Modules/VerifyPhoneModule/CountryCode/CountryCodeViewController.swift
@@ -41,8 +41,8 @@ class CountryCodeViewController: UIViewController {
         loadData()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         searchBar.becomeFirstResponder()
     }
 
